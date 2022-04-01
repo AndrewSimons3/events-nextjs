dbf15780054af045c51849bfb4d852fad1ab0c00
AndrewSimons3 <simdrew07@gmail.com>
2022-04-01 09:00:42 -0500

    events pages created

---
+++ events-nextjs/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FiltertedEventsPage = () => {
+  return (
+    <div>
+      <h1>This is the Filtered Events Page</h1>
+    </div>
+  )
+}
+
+export default FiltertedEventsPage;
---
+++ events-nextjs/pages/index.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+  return (
+    <div><h1>This is the Home Page</h1></div>
+  )
+}
+
+export default HomePage;
